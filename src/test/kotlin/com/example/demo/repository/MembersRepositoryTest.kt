cb638a6933f2d30f18d9440462bfdab8c4b30041
jisoojung <jisoo.jung@dramancompany.com>
2022-07-03 23:48:48 +0900

    detekt 수정사항 반영

--- src/test/kotlin/com/example/demo/repository/MembersRepositoryTest.kt
+++ src/test/kotlin/com/example/demo/repository/MembersRepositoryTest.kt
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MembersRepositoryTest @Autowired constructor (
+class MembersRepositoryTest @Autowired constructor(
     private val membersRepository: MembersRepository
 ) {
 
