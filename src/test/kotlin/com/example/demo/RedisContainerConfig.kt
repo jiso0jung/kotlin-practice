c92f8f2e1354ebaaedaed0b151d094cdd457670f
jisoojung <jisoo.jung@dramancompany.com>
2022-07-04 10:16:57 +0900

    testcontainers 불필요 설정 삭제

--- src/test/kotlin/com/example/demo/RedisContainerConfig.kt
+++ src/test/kotlin/com/example/demo/RedisContainerConfig.kt
@@ -2,14 +2,9 @@ package com.example.demo
 
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 
-
-@Testcontainers
-open class RedisContainerConfig {
+abstract class RedisContainerConfig {
     companion object {
-        @Container
         var REDIS_CONTAINER: KGenericContainer = KGenericContainer("redis:7-alpine").withExposedPorts(6379)
 
         @JvmStatic
