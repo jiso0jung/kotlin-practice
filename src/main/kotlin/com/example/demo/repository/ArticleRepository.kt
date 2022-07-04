ab184709a222abd232d769a837f269053f02d8ac
jisoojung <jisoo.jung@dramancompany.com>
2022-07-04 18:08:49 +0900

    CrudRepository -> JpaRepository

--- src/main/kotlin/com/example/demo/repository/ArticleRepository.kt
+++ src/main/kotlin/com/example/demo/repository/ArticleRepository.kt
@@ -1,9 +1,9 @@
 package com.example.demo.repository
 
 import com.example.demo.domain.Article
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface ArticleRepository : CrudRepository<Article, Long> {
+interface ArticleRepository : JpaRepository<Article, Long> {
     fun findBySlug(slug: String): Article?
     fun findAllByOrderByAddedAtDesc(): Iterable<Article>
 }
--- src/main/kotlin/com/example/demo/repository/UsersRepository.kt
+++ src/main/kotlin/com/example/demo/repository/UsersRepository.kt
@@ -1,8 +1,8 @@
 package com.example.demo.repository
 
 import com.example.demo.domain.Users
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 
-interface UsersRepository : CrudRepository<Users, Long> {
+interface UsersRepository : JpaRepository<Users, Long> {
     fun findByLogin(login: String): Users?
 }
